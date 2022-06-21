9e93cbd857f3f8880ff3fdffdcb15add07f9a4f7
tubagusaom <tubagus.aom.swk@gmail.com>
2022-06-21 23:57:18 +0700

    commitmac

--- wee/application/config/config.php
+++ wee/application/config/config.php
@@ -15,11 +15,11 @@
 |
 */
 
-$config['base_url'] = "https://" . $_SERVER['HTTP_HOST'] . str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+// $config['base_url'] = "https://" . $_SERVER['HTTP_HOST'] . str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 
-// $_SERVER['HTTP_HOST'] . str_replace(basename($_SERVER['SCRIPT_NAME']), "",
-// $_SERVER['SCRIPT_NAME']);
-// $config['base_url']	= '';
+$_SERVER['HTTP_HOST'] . str_replace(basename($_SERVER['SCRIPT_NAME']), "",
+$_SERVER['SCRIPT_NAME']);
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
